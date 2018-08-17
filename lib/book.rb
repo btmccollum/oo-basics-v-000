45da4c3e77e0c0043b00b67971b10cb07b48cabe
Brad McCollum <btmcco8@gmail.com>
2018-08-17 16:36:02 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -12,39 +12,39 @@ class Book
 end
 
 # class Book #manually writing out the attr_accessor/readers
-  # 
+  #
   # def initialize(title)
   #   @title = title
   # end
-  # 
+  #
   # def title
   #   @title
   # end
-  # 
+  #
   # def author=(author)
   #   @author = author
   # end
-  # 
+  #
   # def author
   #   @author
   # end
-  # 
+  #
   # def page_count=(page_count)
   #   @page_count = page_count
   # end
-  # 
+  #
   # def page_count
   #   @page_count
   # end
-  # 
+  #
   # def genre=(genre)
   #   @genre = genre
   # end
-  # 
+  #
   # def genre
   #   @genre
   # end
-  # 
+  #
   # def turn_page
   #   puts "Flipping the page...wow, you read fast!"
   # end
